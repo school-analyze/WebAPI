45f69f747ff9cff799e5a0498f03e3bddddc722c
Firest <rohozin.max@gmail.com>
2025-01-23 12:46:38 +0100

    Created class for endpoints

--- AnalyzeAPI/Program.cs
+++ AnalyzeAPI/Program.cs
@@ -25,30 +25,7 @@ app.UseHttpsRedirection();
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnalyzeAPI v1"));
 
-app.MapGet("/", () => "Data Analyze API"); // All marks get
-app.MapPost("/marks/add" , (int mark) => {
-    return mark;
-}); // Add mark
-app.MapDelete("/marks/delete/{id}", (int id) => {
-    return id;
-}); // Delete mark
-app.MapPut("/marks/update/{id}", (int id, int mark) => {
-    return id + " " + mark;
-}); // Update mark
 
-app.MapGet("/users", () => "Users API"); // All users get
-app.MapGet("/users/{id}", (int id) => {
-    return id;
-}); // User get
-app.MapPost("/users/add", (string name) => {
-    return name;
-}); // Add user
-app.MapDelete("/users/delete/{id}", (int id) => {
-    return id;
-}); // Delete user
-app.MapPut("/users/update/{id}", (int id, string name) => {
-    return id + " " + name;
-}); // Update user
 
 
 app.Run();
---
+++ AnalyzeAPI/ProgramEndpoints.cs
@@ -0,0 +1,32 @@
+﻿namespace AnalyzeAPI;
+
+public class ProgramEndpoints
+{
+    public static void MapEndpoints(WebApplication app)
+    {
+        app.MapGet("/", () => "Data Analyze API"); // All marks get
+        app.MapPost("/marks/add" , (int mark) => {
+            return mark;
+        }); // Add mark
+        app.MapDelete("/marks/delete/{id}", (int id) => {
+            return id;
+        }); // Delete mark
+        app.MapPut("/marks/update/{id}", (int id, int mark) => {
+            return id + " " + mark;
+        }); // Update mark
+
+        app.MapGet("/users", () => "Users API"); // All users get
+        app.MapGet("/users/{id}", (int id) => {
+            return id;
+        }); // User get
+        app.MapPost("/users/add", (string name) => {
+            return name;
+        }); // Add user
+        app.MapDelete("/users/delete/{id}", (int id) => {
+            return id;
+        }); // Delete user
+        app.MapPut("/users/update/{id}", (int id, string name) => {
+            return id + " " + name;
+        }); // Update user
+    }
+}
