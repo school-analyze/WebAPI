af4bff9965e4b08647b49ca7c97b605b8b662e57
Firest <rohozin.max@gmail.com>
2025-01-23 12:36:55 +0100

    Added endpoints to do

--- AnalyzeAPI/Program.cs
+++ AnalyzeAPI/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnalyzeAPI", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,5 +21,34 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnalyzeAPI v1"));
+
+app.MapGet("/", () => "Data Analyze API"); // All marks get
+app.MapPost("/marks/add" , (int mark) => {
+    return mark;
+}); // Add mark
+app.MapDelete("/marks/delete/{id}", (int id) => {
+    return id;
+}); // Delete mark
+app.MapPut("/marks/update/{id}", (int id, int mark) => {
+    return id + " " + mark;
+}); // Update mark
+
+app.MapGet("/users", () => "Users API"); // All users get
+app.MapGet("/users/{id}", (int id) => {
+    return id;
+}); // User get
+app.MapPost("/users/add", (string name) => {
+    return name;
+}); // Add user
+app.MapDelete("/users/delete/{id}", (int id) => {
+    return id;
+}); // Delete user
+app.MapPut("/users/update/{id}", (int id, string name) => {
+    return id + " " + name;
+}); // Update user
+
 
 app.Run();
--- AnalyzeAPI/obj/Debug/net9.0/AnalyzeAPI.AssemblyInfo.cs
+++ AnalyzeAPI/obj/Debug/net9.0/AnalyzeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnalyzeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04313bbd4845c07119cbd079112420ecd6a0aaf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnalyzeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnalyzeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
