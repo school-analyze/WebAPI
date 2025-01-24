d71c88b4a09ec65b6d250256dc27b0b9219179c8
Firest <rohozin.max@gmail.com>
2025-01-24 13:27:14 +0100

    Http requests for testing

--- AnalyzeAPI/obj/Debug/net9.0/AnalyzeAPI.AssemblyInfo.cs
+++ AnalyzeAPI/obj/Debug/net9.0/AnalyzeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnalyzeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a96392c787fff3be701049b9f3ac53f59a6820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c61b07825e0ef30ff71c52b4229e7a64488265f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnalyzeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnalyzeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
